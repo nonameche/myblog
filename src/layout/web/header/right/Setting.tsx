042ac241bb7f54e28ab0dcae4442e189bf6b09ad
limeng <946145605@qq.com>
2021-03-27 19:32:40 +0800

    🐛 解决黑夜模式显示问题

--- src/layout/web/header/right/Setting.tsx
+++ src/layout/web/header/right/Setting.tsx
@@ -8,13 +8,13 @@ interface BaseProps{
 
 export const Setting:FC<BaseProps> = () => {
   // const [isDarkMode, setIsDarkMode] = useState(false)
-  const darkMode = useDarkMode()
+  const darkMode = useDarkMode(false)
   console.log('🚀 ~ file: Setting.tsx ~ line 12 ~ darkMode', darkMode.value)
   return (
     <div className='menu'>
       <DarkModeToggle
         onChange={darkMode.toggle}
-        checked={!darkMode.value}
+        checked={darkMode.value}
         size={40}
       />
     </div>
